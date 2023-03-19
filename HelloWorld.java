f1b72c0ad2875f3423f79a136f5085bfb4e8b0c9
dhirtiman <dhirtimankhati@gmail.com>
2023-03-19 14:05:51 +0530

    go

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+
+
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My other Java Program");
+
+        String dk;
+        dk = "dhirtiman khati";
+        System.out.println(dk);
+
+        int dkEarn = 500;
+        int yearly = dkEarn*4;
+        System.out.println(yearly);
+
+    }      
+}
--- Tutorial/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package Tutorial;
-
-
-public class HelloWorld{
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("My other Java Program");
-    }
-}
