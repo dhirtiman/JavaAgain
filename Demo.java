25a6455d8f9fbc7809780b62c612415d23d99447
dhirtiman <dhirtimankhati@gmail.com>
2023-03-19 20:35:16 +0530

    this.this thing

--- Demo.java
+++ Demo.java
@@ -2,21 +2,13 @@ public class Demo {
     public static void main(String[] args) {
         Player dk = new Player();
         System.out.println(dk.getHandleName());
-        dk.setHandleName("Dhirtiman");
+
+
+        dk.setNameAndLevel("Dhirtiman",5);
         System.out.println(dk.getHandleName());
-        System.out.println("Level: "+dk.getLevels());
-        System.out.println("Lives: "+dk.getLives());
+        System.out.println("Level: " + dk.getlevel());
+        System.out.println("Lives: " + dk.getLives());
 
-        Player pk = new Player("pkay");
-        System.out.println(pk.getHandleName());
-        pk.setLives(5);
-        System.out.println("Level: "+pk.getLevels());
-        System.out.println("Lives: "+pk.getLives());
-
-        Player bk = new Player("bkay",6);
-        System.out.println(bk.getHandleName());
-        System.out.println("Level: "+bk.getLevels());
-        System.out.println("Lives: "+bk.getLives());
     }
-    
+
 }
--- Player.java
+++ Player.java
@@ -1,6 +1,6 @@
 public class Player {
     private String handleName;
-    private int lives, levels, score;
+    private int lives, level, score;
 
     public int getLives() {
         return lives;
@@ -10,12 +10,12 @@ public class Player {
         this.lives = lives;
     }
 
-    public int getLevels() {
-        return levels;
+    public int getlevel() {
+        return level;
     }
 
-    public void setLevels(int levels) {
-        this.levels = levels;
+    public void setlevel(int level) {
+        this.level = level;
     }
 
     public int getScore() {
@@ -26,37 +26,50 @@ public class Player {
         this.score = score;
     }
 
+
+
     public Player() {
         this("PlayerMan");
     }
 
-    public Player(String handle){
+    public Player(String handle) {
         this(handle, 1);
         // handleName = handle;
         // lives = 3;
-        // levels = 1;
+        // level = 1;
         // score = 0;
 
     }
 
-    public Player(String handle, int startingLevel){
-        handleName = handle;
-        lives = 3;
-        levels = startingLevel;
-        score = 0;
+    public Player(String handle, int startingLevel) {
+        // this.handleName = handle;
+        // this.lives = 3;
+        // this.level = startingLevel;
+        // this.score = 0;
+        setHandleName(handle);
+        setLives(3);
+        setlevel(startingLevel);
+        setScore(0);
+
     }
 
-    public String getHandleName(){
+    public String getHandleName() {
         return handleName;
     }
-    public void setHandleName(String handle){
-        if (handle.length()<3){
-            System.out.println("To Short!");
+
+    public void setHandleName(String handle) {
+        if (handle.length() < 3) {
+            System.out.println(handle+" To Short!");
             return;
         }
-        handleName = handle;
+        this.handleName = handle;
     }
-    
 
+    public void setNameAndLevel(String name,int level){
+        // this.handleName = name;
+        // this.level = level;
+        setHandleName(name);
+        setlevel(level);
+    }
 
 }
